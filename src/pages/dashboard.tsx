0875f967cad3d6dbdb996050204824f310df055a
MichaelJuannn <juan600d@gmail.com>
2023-02-07 12:32:51 +0700

    dashboard creation

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+	return (
+		<>
+			<nav className='bg-base-100 border-main p-3 rounded'>
+				<div className='container'>
+					<div className=''>Title</div>
+				</div>
+			</nav>
+		</>
+	);
+}
